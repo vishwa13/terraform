b46751c6ee7b804ebd2f45e75b35aed09a1dc856
Vishwanatha bhat <bhatvishwa363@gmail.com>
2023-02-27 13:39:17 +0530

    added list of providers file

--- main.tf
+++ main.tf
@@ -1,16 +1,23 @@
 provider "aws"{
     region= "ap-south-1"
-    access_key="AKIAZZMXQKZ7GFJGZHOT"
-    secret_key="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"
+
 }
 
 
 resource "aws_vpc" "development-vpc"{
     cidr_block="10.0.0.0/16"
+    tags={
+        Name:"Development-vpc"
+        vpc_env:"dev"
+    }
 }
 
 resource "aws_subnet" "development-sub-1"{
     vpc_id=aws_vpc.development-vpc.id
     cidr_block="10.0.10.0/24"
     availability_zone="ap-south-1a"
+    tags={
+        Name:"Dev-subnet-1"
+       
+    }
 }
